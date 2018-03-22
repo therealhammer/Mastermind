69d61ca8ce6065d892bdc638a2e7d15d6861c391
Spencer Young <spencer.young@etes.de>
2018-03-22 13:23:18 +0100

    Hart kodierte Strings extrahiert, Einstellungs-Activity hinzugefügt

---
+++ app/src/main/java/de/dhbw/tinf16k/mastermind/Einstellungen.java
@@ -0,0 +1,12 @@
+package de.dhbw.tinf16k.mastermind;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Einstellungen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.einstellungen);
+    }
+}
