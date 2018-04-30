749992009802577cd79763d28a8936789eee4b0b
Thierry Hammer <freddymaerz@googlemail.com>
2018-04-30 10:52:28 +0200

    Versionierung

--- app/src/main/java/de/dhbw/tinf16k/mastermind/Licenses.java
+++ app/src/main/java/de/dhbw/tinf16k/mastermind/Licenses.java
@@ -5,12 +5,16 @@ package de.dhbw.tinf16k.mastermind;
  */
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Licenses extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.licenses);
+        TextView tv = (TextView)findViewById(R.id.version);
+        tv.setText("Version: " + BuildConfig.VERSION_NAME);
     }
 }
