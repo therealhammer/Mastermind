6c404c2044cde467e57fa552d47baccf4956a6f2
Thierry Hammer <freddymaerz@googlemail.com>
2018-05-04 13:03:44 +0200

    Versionierung

--- app/src/main/java/de/dhbw/tinf16k/mastermind/Rules.java
+++ app/src/main/java/de/dhbw/tinf16k/mastermind/Rules.java
@@ -6,11 +6,18 @@ package de.dhbw.tinf16k.mastermind;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.webkit.WebView;
+
+import java.io.File;
 
 public class Rules extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rules);
+        /*WebView browser = (WebView) findViewById(R.id.webV);
+        File webFile = new File(Environment.getDataDirectory() + "/test.html");
+        browser.loadUrl("file:///" + webFile.getAbsolutePath());*/
     }
 }
